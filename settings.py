96fb3bc64c6bd20d036307116545f0191441c59e
Nemoni <490202694@qq.com>
2017-06-30 00:14:50 +0800

    initialize the Python project Alient Invasion

---
+++ settings.py
@@ -0,0 +1,9 @@
+class Settings():
+
+    def __init__(self):
+        # 屏幕宽高
+        self.screen_width = 1200
+        self.screen_height = 800
+        # 设置背景色
+        self.bg_color = (255, 204, 255)
+        
